dfcc57a17b20a9da5455357de6048321322ef28e
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2024-02-29 01:15:16 +0600

    Middlewares - initialize

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+//Middleware Functions
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 5000;
+app.listen(port, (req, res) => {
+  console.log(`Server is running on port: ${port}`);
+});
