078b4298ded1b3f5c3864bc657d76266477f859f
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2024-02-29 14:55:07 +0600

    Mongoose Connected

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 
@@ -8,6 +10,16 @@ app.use(express.json());
 app.use(cors());
 
 const port = process.env.PORT || 5000;
+const uri = process.env.DATABASE_URI;
+
 app.listen(port, (req, res) => {
-  console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port} ....`);
 });
+
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connection established..."))
+  .catch((error) => console.error("MongoDB connection failed:", error.message));
