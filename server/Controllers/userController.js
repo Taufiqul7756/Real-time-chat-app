d498b76c7ee6ee0791a015288dfcf0f7b0abef7a
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2024-02-29 16:34:21 +0600

    Register routes and functionality

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -1,7 +1,46 @@
 const userModel = require("../Models/userModel");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
+const jwt = require("jsonwebtoken");
 
-const registerUser = (req, res) => {
-  res.send("Register......");
+const createToken = (_id) => {
+  const jwtkey = process.env.JWT_SECRET_KEY;
+  return jwt.sign({ _id }, jwtkey, { expiresIn: "3d" });
 };
 
-module.exports = { registerUser };
+const registerUser = async (req, res) => {
+  try {
+    const { first_name, last_name, email, password } = req.body;
+
+    let user = await userModel.findOne({ email });
+
+    if (user) return res.status(400).json("User already Exit...");
+
+    if (!first_name || !last_name || !email || !password)
+      return res.status(400).json("All fields are required");
+
+    if (!validator.isEmail(email))
+      return res.status(400).json("Email must be Valid");
+
+    if (!validator.isStrongPassword(password))
+      return res.status(400).json("Password must be Strong");
+
+    user = new userModel({ first_name, last_name, email, password });
+
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+
+    await user.save();
+
+    const token = createToken(user._id);
+
+    res
+      .status(200)
+      .json({ _id: user._id, first_name, last_name, email, token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
+
+module.exports = { registerUser, loginUser };
