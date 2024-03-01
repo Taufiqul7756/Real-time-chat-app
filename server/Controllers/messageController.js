514a51cbd338905c3541246637ce33914ae49d9d
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2024-03-02 01:44:48 +0600

    getMessages

--- server/Controllers/messageController.js
+++ server/Controllers/messageController.js
@@ -22,4 +22,17 @@ const createMessage = async (req, res) => {
 
 // getMessage
 
-module.exports = { createMessage };
+const getMessages = async (req, res) => {
+  const { chatId } = req.params;
+
+  try {
+    const messages = await messageModel.find({ chatId });
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
+
+module.exports = { createMessage, getMessages };
