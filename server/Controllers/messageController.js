a8c33ca2612ade45f7825b4f455fb1d1b2505728
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2024-03-02 01:42:28 +0600

    messageController - createMessage

---
+++ server/Controllers/messageController.js
@@ -0,0 +1,25 @@
+const messageModel = require("../Models/messageModel");
+
+//CreateMessage
+
+const createMessage = async (req, res) => {
+  const { chatId, senderId, text } = req.body;
+
+  const message = new messageModel({
+    chatId,
+    senderId,
+    text,
+  });
+
+  try {
+    const response = await message.save();
+    res.status(200).json(response);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
+
+// getMessage
+
+module.exports = { createMessage };
