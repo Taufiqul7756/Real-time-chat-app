511e3658959ef584c981dbee01c5e407856c307b
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2024-03-01 23:12:49 +0600

    Register user to Database

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,101 +1,95 @@
-import { useForm } from "react-hook-form";
+import { useContext } from "react";
+import { Alert, Button, Col, Form, Row, Stack } from "react-bootstrap";
+import { AuthContext } from "../context/AuthContext";
 
-const RegisterForm = () => {
+const Register = () => {
   const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-
-  const onSubmit = (data) => {
-    console.log(data);
-    // You can handle form submission logic here, such as sending data to the server
-  };
+    registerInfo,
+    updateRegisterInfo,
+    registerUser,
+    registerError,
+    isRegisterLoading,
+  } = useContext(AuthContext);
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="max-w-md mx-auto mt-8 p-4 border rounded-lg shadow-lg"
-    >
-      <h2 className="text-2xl font-bold mb-4">Register</h2>
-      <div className="mb-4 flex">
-        <div className="w-1/2 mr-2">
-          <label
-            htmlFor="firstName"
-            className="block text-gray-700 font-semibold mb-2"
-          >
-            First Name
-          </label>
-          <input
-            type="text"
-            id="firstName"
-            {...register("firstName", { required: "First name is required" })}
-            className="form-input bg-gray-100 text-black border-2 border-gray-300 focus:outline-none focus:border-blue-500 w-full"
-          />
-          {errors.firstName && (
-            <span className="text-red-500">{errors.firstName.message}</span>
-          )}
-        </div>
-        <div className="w-1/2 ml-2">
-          <label
-            htmlFor="lastName"
-            className="block text-gray-700 font-semibold mb-2"
-          >
-            Last Name
-          </label>
-          <input
-            type="text"
-            id="lastName"
-            {...register("lastName", { required: "Last name is required" })}
-            className="form-input text-black bg-gray-100 border-2 border-gray-300 focus:outline-none focus:border-blue-500 w-full"
-          />
-          {errors.lastName && (
-            <span className="text-red-500">{errors.lastName.message}</span>
-          )}
-        </div>
-      </div>
-      <div className="mb-4">
-        <label
-          htmlFor="email"
-          className="block text-gray-700 font-semibold mb-2"
+    <>
+      <Form onSubmit={registerUser}>
+        <Row
+          style={{
+            height: "100vh",
+            justifyContent: "center",
+            paddingTop: "20%",
+          }}
         >
-          Email
-        </label>
-        <input
-          type="email"
-          id="email"
-          {...register("email", { required: "Email is required" })}
-          className="form-input text-black bg-gray-100 border-2 border-gray-300 focus:outline-none focus:border-blue-500 w-full"
-        />
-        {errors.email && (
-          <span className="text-red-500">{errors.email.message}</span>
-        )}
-      </div>
-      <div className="mb-4">
-        <label
-          htmlFor="password"
-          className="block text-gray-700 font-semibold mb-2"
-        >
-          Password
-        </label>
-        <input
-          type="password"
-          id="password"
-          {...register("password", { required: "Password is required" })}
-          className="form-input text-black bg-gray-100 border-2 border-gray-300 focus:outline-none focus:border-blue-500 w-full"
-        />
-        {errors.password && (
-          <span className="text-red-500">{errors.password.message}</span>
-        )}
-      </div>
-      <button
-        type="submit"
-        className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded"
-      >
-        Register
-      </button>
-    </form>
+          <Col xs={6}>
+            <Stack gap={3}>
+              <h2>Register</h2>
+
+              <Form.Control
+                type="text"
+                placeholder="First Name"
+                onChange={(e) =>
+                  updateRegisterInfo({
+                    ...registerInfo,
+                    first_name: e.target.value,
+                  })
+                }
+                value={registerInfo.first_name}
+                required
+              />
+              <Form.Control
+                type="text"
+                placeholder="Last Name"
+                onChange={(e) =>
+                  updateRegisterInfo({
+                    ...registerInfo,
+                    last_name: e.target.value,
+                  })
+                }
+                value={registerInfo.last_name}
+                required
+              />
+              <Form.Control
+                type="email"
+                placeholder="Email"
+                onChange={(e) =>
+                  updateRegisterInfo({ ...registerInfo, email: e.target.value })
+                }
+                value={registerInfo.email}
+                required
+              />
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                onChange={(e) =>
+                  updateRegisterInfo({
+                    ...registerInfo,
+                    password: e.target.value,
+                  })
+                }
+                value={registerInfo.password}
+                required
+              />
+              <Button
+                variant="primary"
+                type="submit"
+                disabled={isRegisterLoading}
+              >
+                {isRegisterLoading ? "Creating your account..." : "Register"}
+              </Button>
+
+              {registerError?.error && (
+                <Alert variant="danger">
+                  <b>{`Error status code: ${registerError?.status}`}</b>
+                  <p>{registerError?.message}</p>
+                </Alert>
+              )}
+            </Stack>
+          </Col>
+        </Row>
+      </Form>
+    </>
   );
 };
 
-export default RegisterForm;
+export default Register;
