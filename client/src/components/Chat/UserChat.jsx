dd41db0d99f5a3cf3d2383dc36511b74af8c5355
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2024-03-03 02:17:13 +0600

    User list showing on UI - active status , name, fake date , fake unreadMessage

---
+++ client/src/components/Chat/UserChat.jsx
@@ -0,0 +1,37 @@
+// import React from "react";
+
+import { Stack } from "react-bootstrap";
+import { useFetchRecipientUser } from "../../hooks/useFetchRecipent";
+import avatar from "../../assets/avarter.svg";
+
+// eslint-disable-next-line react/prop-types
+const UserChat = ({ chat, user }) => {
+  const { recipientUser } = useFetchRecipientUser(chat, user);
+
+  return (
+    <Stack
+      direction="horizontal"
+      gap={3}
+      className="user-card align-items-center p-2 justify-content-between"
+      role="button"
+    >
+      <div className="d-flex">
+        <div className="me-2">
+          <img src={avatar} height="35px" width="35px" alt="" />
+        </div>
+        <div className="text-content">
+          <div className="name">{recipientUser?.first_name}</div>
+          <div className="text"> Test Messages </div>
+        </div>
+      </div>
+
+      <div className="d-flex flex-column align-items-end">
+        <div className="date">12/12/24</div>
+        <div className="this-user-notifications">2</div>
+        <span className="user-online"></span>
+      </div>
+    </Stack>
+  );
+};
+
+export default UserChat;
--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,18 +1,42 @@
-// import React from "react";
-
+/* eslint-disable no-unused-vars */
 import { useContext } from "react";
-import { ChatContext } from "../context/ChatContext";
+import { Container, Stack } from "react-bootstrap";
+// import AllUsers from "../components/Chat/AllUsers";
+// import ChatBox from "../components/Chat/ChatBox";
+// import UserCard from "../components/Chat/UserChat";
 import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
+import UserChat from "../components/Chat/UserChat";
 
 const Chat = () => {
   const { user } = useContext(AuthContext);
 
-  const { userChats, isUserChatsLoading, updateCurrentChat } =
+  const { userChats, isUserChatsLoading, userChatsError } =
     useContext(ChatContext);
 
-  console.log("userChats----", userChats);
   return (
-    <div className="text-3xl text-black font-bold underline p-20">Chat</div>
+    <Container>
+      {userChats?.length < 1 ? null : (
+        <div className="flex flex-col md:flex-row">
+          <div className="flex-grow-0 pe-3">
+            <Stack direction="horizontal" gap={3} className="align-items-start">
+              <Stack className="messages-box flex-grow-8 pe-3" gap={3}>
+                {isUserChatsLoading && <p>Loading CHats...</p>}
+
+                {userChats?.map((chat, index) => {
+                  return (
+                    <div key={index}>
+                      <UserChat chat={chat} user={user} />
+                    </div>
+                  );
+                })}
+              </Stack>
+              <p>Chatbox</p>
+            </Stack>
+          </div>
+        </div>
+      )}
+    </Container>
   );
 };
 
