51da82a153ad55ea8677bd860f74ee82c9175711
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2024-03-04 00:08:57 +0600

    Making Functionalities for online users

--- socket/index.js
+++ socket/index.js
@@ -1,11 +1,29 @@
-import { Server } from "socket.io";
+const { Server } = require("socket.io");
 
-const io = new Server({
-  /* options */
-});
+const io = new Server({ cors: "http://localhost:5173/" });
+
+let onlineUsers = [];
 
 io.on("connection", (socket) => {
-  // ...
+  console.log("New Connection: ", socket.id);
+
+  // listen to a connection
+  socket.on("AddNewUser", (userId) => {
+    // check user is online or not , if not then push it to array
+    !onlineUsers.some((user) => user.userId === userId) &&
+      onlineUsers.push({
+        userId,
+        socketId: socket.id,
+      });
+  });
+
+  console.log("Online users:", onlineUsers);
+  io.emit("getOnlineUsers", onlineUsers);
+
+  socket.on("disconnect", () => {
+    onlineUsers = onlineUsers.filter((user) => user.socketId !== socket.id);
+    io.emit("getOnlineUsers", onlineUsers);
+  });
 });
 
-io.listen(3000);
+io.listen(8080);
