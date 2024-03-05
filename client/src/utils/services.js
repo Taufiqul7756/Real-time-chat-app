930b4fe341b81cb6d5a96c45efdf13777f3756d0
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2024-03-05 15:42:20 +0600

    error handling

--- client/src/utils/services.js
+++ client/src/utils/services.js
@@ -27,21 +27,24 @@ export const postRequest = async (url, body) => {
 };
 
 export const getRequest = async (url) => {
-  const response = await fetch(url);
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
 
-  const data = await response.json();
+    if (!response.ok) {
+      let message = "An error occurred";
 
-  if (!response.ok) {
-    let message = "An error occured...";
+      if (data?.message) {
+        message = data.message;
+      }
 
-    if (data?.message) {
-      message = data.message;
+      return { error: true, status: response.status, message };
     }
 
-    return { error: true, status: response.status, message };
+    return data;
+  } catch (error) {
+    return { error: true, message: error.message };
   }
-
-  return data;
 };
 
 // export const deleteRequest = async (url) => {
