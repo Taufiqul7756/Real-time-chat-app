dc2c71effdade5c74a5ee14d5a0cdcb32ba65f65
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2024-03-05 14:53:13 +0600

    Delete route for individual message deleting by their respective messageId

--- server/Controllers/messageController.js
+++ server/Controllers/messageController.js
@@ -35,4 +35,22 @@ const getMessages = async (req, res) => {
   }
 };
 
-module.exports = { createMessage, getMessages };
+// DeleteMessage
+const deleteMessage = async (req, res) => {
+  const { messageId } = req.params;
+
+  try {
+    const deletedMessage = await messageModel.findByIdAndDelete(messageId);
+
+    if (!deletedMessage) {
+      return res.status(404).json({ message: "Message not found" });
+    }
+
+    res.status(200).json({ message: "Message deleted successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
+
+module.exports = { createMessage, getMessages, deleteMessage };
--- server/Routes/messageRoute.js
+++ server/Routes/messageRoute.js
@@ -2,11 +2,13 @@ const express = require("express");
 const {
   createMessage,
   getMessages,
+  deleteMessage,
 } = require("../Controllers/messageController");
 
 const router = express.Router();
 
 router.post("/", createMessage);
 router.get("/:chatId", getMessages);
+router.delete("/:messageId", deleteMessage);
 
 module.exports = router;
